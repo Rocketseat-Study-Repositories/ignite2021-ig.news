f82f5ed2a7f99e02df45b86cbef941d4b0fb01eb
Bruno Girome <brunogirome@gmail.com>
2021-06-08 21:20:01 -0300

    Added trycatch for fauna insertion

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -16,9 +16,13 @@ export default NextAuth({
     async signIn(user, account, profile) {
       const { email } = user;
 
-      await fauna.query(q.Create(q.Collection('users'), { data: { email } }));
+      try {
+        await fauna.query(q.Create(q.Collection('users'), { data: { email } }));
 
-      return true;
+        return true;
+      } catch (error) {
+        return false;
+      }
     },
   },
 });
