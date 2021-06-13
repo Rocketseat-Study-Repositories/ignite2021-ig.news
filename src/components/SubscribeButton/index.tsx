3bd5efc47d5587e69af6ba86987dab20400dd6f3
Bruno Girome <brunogirome@gmail.com>
2021-06-12 22:24:22 -0300

    Basic user creation and checkout page redirect from stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,7 @@
 import { useSession, signIn } from 'next-auth/client';
+import { getStripeJs } from '../../services/stripe-js';
+import { api } from '../../services/api';
+
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -8,10 +11,22 @@ interface SubscribeButtonProps {
 export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
   const [session] = useSession();
 
-  function HandleSubscribe() {
+  async function HandleSubscribe() {
     if (!session) {
       signIn('github');
       return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert(error.message);
     }
   }
 
